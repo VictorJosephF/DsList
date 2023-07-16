86e269a48b07bc3a13bbbd58e640f9495c9d811f
VictorJosephF <victorfariaj@gmail.com>
2023-07-16 20:51:52 -0300

    Get game by ID

--- src/main/java/com/victorfaria/dslist/Controllers/GameController.java
+++ src/main/java/com/victorfaria/dslist/Controllers/GameController.java
@@ -4,9 +4,11 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.victorfaria.dslist.DTO.GameDTO;
 import com.victorfaria.dslist.DTO.GameMinDTO;
 import com.victorfaria.dslist.services.GameService;
 
@@ -20,6 +22,12 @@ public class GameController {
 	@GetMapping
 	public List<GameMinDTO> findAll(){
 		List<GameMinDTO> result = gameService.findall();
+		return result;
+	}
+	
+	@GetMapping(value = "/{id}")
+	public GameDTO findById(@PathVariable Long id){
+		GameDTO result = gameService.findById(id);
 		return result;
 	}
 }
--- src/main/java/com/victorfaria/dslist/services/GameService.java
+++ src/main/java/com/victorfaria/dslist/services/GameService.java
@@ -3,7 +3,9 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.victorfaria.dslist.DTO.GameDTO;
 import com.victorfaria.dslist.DTO.GameMinDTO;
 import com.victorfaria.dslist.entities.Game;
 import com.victorfaria.dslist.repositories.GameRepository;
@@ -13,6 +15,13 @@ public class GameService {
 	
     @Autowired
     private GameRepository gameRepository;
+    
+    @Transactional(readOnly = true)
+    public GameDTO findById(Long id) {
+    	Game result = gameRepository.findById(id).get();
+    	GameDTO dto = new GameDTO(result);
+    	return dto;
+    }
     
 	public List<GameMinDTO> findall(){
 		List<Game> result = gameRepository.findAll();
